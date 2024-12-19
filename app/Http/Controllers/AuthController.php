5779f1b2cc9831879aa447c680fab53e2cdc3cec
AdamRse <bakaarion3@gmail.com>
2024-12-19 15:15:43 +0100

    Login et register ok

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller {
+
+    function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'firstname' => 'required|string|max:63',
+            'lastname' => 'required|string|max:127',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => 'Validation failed',
+                'messages' => $validator->errors()
+            ], 422);
+        }
+
+        $user = User::create([
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User successfully registered',
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ], 201);
+    }
+
+    function login(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => 'Validation failed',
+                'messages' => $validator->errors()
+            ], 422);
+        }
+
+        // Tentative de connexion
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'error' => 'Invalid credentials'
+            ], 401);
+        }
+
+        /** @var User $user */
+        $user = Auth::user();
+
+        $user->tokens()->delete(); //On supprime les anciens tokens
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Successfully logged in',
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ], 200);
+    }
+}
